c919982fd322cd61d66be4e00aaeebda5650f50c
Filip V <56982870+theowlf@users.noreply.github.com>
2021-01-22 22:18:30 +0100

    added test routes for users, auth, profile and posts

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const usersRoute = require("./routes/api/users");
+const authRoute = require("./routes/api/auth");
+const profileRoute = require("./routes/api/profile");
+const postsRoute = require("./routes/api/posts");
 
 const app = express();
 
@@ -9,5 +13,11 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("API Running"));
+
+// Define Routes
+app.use("/api/users", usersRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/posts", postsRoute);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
